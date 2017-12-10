ad27cf060dd0e3f2e3c9d6458d8e4b6a3c286ce7
Matheus Diniz Rodrigues <mdrwtf@gmail.com>
2017-12-10 14:58:06 -0200

    Fibonacci Sequencer Complete (Minimal style)

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const number = document.getElementById('number');
+const submit = document.getElementById('submit');
+const output = document.getElementById('outp');
+
+
+function fibonacci(){
+    let iteration = number.value;
+    var n1 = 0;
+    var n2 = 1;
+    var temp;
+    var array = []
+    for(i = 0; i < iteration; i++){
+        array.push(n1);
+        temp = n1+n2;
+        n1 = n2;
+        n2 = temp;
+    };
+    // console.log(array);
+    output.innerHTML = array;
+}
